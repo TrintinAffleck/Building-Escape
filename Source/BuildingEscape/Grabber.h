24e39e1c530735ef9d2f1930957b88cb8f97fa72
TrintinAffleck <77417784+TrintinAffleck@users.noreply.github.com>
2021-04-18 15:23:53 -0600

    Add files via upload
    Source code a lot is auto-generated by UE

---
+++ Source/BuildingEscape/Grabber.h
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Components/InputComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "Grabber.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UGrabber : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGrabber();
+
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+private:
+	float Reach = 130.f;
+
+	UPROPERTY()
+	UPhysicsHandleComponent* PhysicsHandle = nullptr;
+	UPROPERTY()
+	UInputComponent* InputComponent = nullptr;
+
+	//Return the first actor within reach with physics body
+	FHitResult GetFirstPhysicsBodyInReach() const;
+	
+	void Grab();
+	void Release();
+	void FindPhysicsHandle();
+	void SetupInputComponent();
+
+	//Return LineTraceEnd
+	FVector GetPlayersReach() const;
+
+	//Get Players Position in world.
+	FVector GetPlayersWorldPos() const;
+};
